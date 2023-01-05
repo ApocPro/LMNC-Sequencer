100dfc56f43009f3f0c5d5fb1288cea0b5ef8c92
Brian <ebwinclil@gmail.com>
2023-01-05 23:21:55 +0100

    LMNC's 2001 Keyboard Sequencer for comparison

--- Keyboard-Seq.ino
+++ Keyboard-Seq.ino
@@ -1,47 +1,43 @@
-//LOOK MUM NO COMPUTER KEYBOARD SEQUENCER. A SIMPLE DESIGN TO MAKE A 4017 STYLE SEQUENCER WITH MORE ADDED FUNCTIONS.
+//LOOK MUM NO COMPUTER KEYBOARD SEQUENCER.
+//VERSION 2.1 (keep an eye out for 2.2 with fixes and extra functions).
+//A SIMPLE DESIGN TO MAKE A 4017 STYLE SEQUENCER WITH MORE ADDED FUNCTIONS.
 //MORE INFO CHECK LOOKMUMNOCOMPUTER.COM
-//PLEASE EMAIL ME COMPUTER@LOOKMUMNOCOMPUTER.COM OR LOOKMUMNOCOMPUTER@GMAIL.COM IF ANY QUESTIONS
-//ANY MODIFICATIONS PLEASE LET ME KNOW AND I CAN FEATURE ON THE WEBSITE
-//OPEN SOURCE. DONT USE IN A PRODUCT OR WHATEVER WITHOUT TALKING TO ME
-//SAM BATTLE 2017
+//JOIN FORUM IF YOU HAVE ANY QUESTIONS
+//ANY MODIFICATIONS PLEASE SHARE ON FORUM PEOPLE MAY ENJOY YOUR TAKE ON THIS
+//SAM BATTLE 2021
 //BOOM
 
+//In Look Mum No Computer Arduino Project Style Its been written in the purest plonker form. 
+//triple distilled plonker code. 
+
+#define STEP1 2  //step 1 output
+#define STEP2 3  //step 2 output
+#define STEP3 4  //step 3 output
+#define STEP4 5  //step 4 output
+#define STEP5 6  //step 5 output
+#define STEP6 7  //step 6 output
+#define STEP7 8  //step 7 output
+#define STEP8 9  //step 8 output
+#define STEPBUTTON1   14   //these are the capacitive touch pads
+#define STEPBUTTON2   15 //these are the capacitive touch pads
+#define STEPBUTTON3   16 //these are the capacitive touch pads
+#define STEPBUTTON4   17 //these are the capacitive touch pads
+#define STEPBUTTON5   18 //these are the capacitive touch pads
+#define STEPBUTTON6   19 //these are the capacitive touch pads
+#define STEPBUTTON7   20 //these are the capacitive touch pads
+#define STEPBUTTON8   21 //these are the capacitive touch pads
+
+#define FORWARDS 12   //this is the pin that the forwards button (and coupled jack input) connects to
+#define RESET 11      //this is the pin that the reset button (and coupled jack input) connects to
+#define BACK 0        //this is the pin that the backwards button (and coupled jack input) connects to
+#define ZERO 10       //this is the pin that the zero button (and coupled jack input) connects to
+#define TOUCHGATE 1   //this is the gate output jack that turns on when a touch pad is being pressed
+#define STEPSET1 13   //this is the pin that sets which row the "merge" jack is listenning to. HIGH is row A, LOW is row B
 
 
-//ALSO TO ADD! I HAVE MADE THIS CODE IN A WAY ITS EASY TO UNDERSTAND FOR A NONE CODER (LIKE MYSELF)
-//I REMEMBER WHEN I STARTED ON ARDUINO BEING LIKE WHAT THE HELL IS GOING ON.
-//SO IVE LAYED IT OUT LOGICALLY, SEINSE IT ISNT A COMPLICATED PROGRAM I FIGURED WHY THE HECK NOT!
-
-//SO YES THIS ISNT THE BEST WAY OF GOING ABOUT THIS HOWEVER IT IS THE EASIEST TO UNDERSTAND
-//FOR A NONE CODER MIND.
-
-
-#define STEP1 2
-#define STEP2 3
-#define STEP3 4
-#define STEP4 5
-#define STEP5 6
-#define STEP6 7
-#define STEP7 8
-#define STEP8 9
-#define STEPBUTTON1   14
-#define STEPBUTTON2   15
-#define STEPBUTTON3   16
-#define STEPBUTTON4   17
-#define STEPBUTTON5   18
-#define STEPBUTTON6   19
-#define STEPBUTTON7   21
-#define STEPBUTTON8   20
-
-#define FORWARDS 12
-#define VARIATION 19
-#define RESET 10
-#define BACK 13
-#define ZERO 11
-
-//ABOVE BASICALLY DEFINES THE NAMES OF THE PINS, PLEASE REMEMBER THE ANALOG PINS A0,A1,A2, BLAH BLAH CAN BE USED AS DIGITAL PINS. THE ARE
-//DIGITAL 14 and UP!
-
+int touchthreshold = 800;  //this is the capacitive touch threshold, go for a higher number for more sensitivity if you have fingers as dry as the sahara
+int Gthrshld = 1000;      //this is the capacitive theshold the gate output will go high
+int capacitivedelay = 0; //delay to smooth the capacitive touch this will change in version 2.2 of the code
 
 int val = 0;
 int old_val = 0;
@@ -80,6 +76,8 @@ int newled = 1;
 
 void setup ()
 {
+  pinMode (STEPSET1, OUTPUT);
+  pinMode (STEP1, OUTPUT);
   pinMode (STEP1, OUTPUT);
   pinMode (STEP2, OUTPUT);
   pinMode (STEP3, OUTPUT);
@@ -92,12 +90,8 @@ void setup ()
   pinMode (BACK, INPUT);
   pinMode (RESET, INPUT);
   pinMode (ZERO, INPUT);
-  pinMode (STEPBUTTON1, INPUT);
-  pinMode (STEPBUTTON2, INPUT);
-  pinMode (STEPBUTTON3, INPUT);
-  pinMode (STEPBUTTON4, INPUT);
-  pinMode (STEPBUTTON5, INPUT);
-  pinMode (STEPBUTTON6, INPUT);
+  pinMode (TOUCHGATE, OUTPUT);
+
 
   //SET THE PINS TO IN OR OUT
   
@@ -107,12 +101,17 @@ void setup ()
 
 void loop ()
 {  
+
+
+
+
+  
   val1 = digitalRead (FORWARDS);
-   if((val1 == HIGH) && (old_val1 == LOW))
+   if((val1 == LOW) && (old_val1 == HIGH))
    
    {
     newled = led - 1; 
-     if (newled <=8) { newled = 16; } 
+     if (newled <=0) { newled = 16; } 
     led = newled;
    
    }
@@ -120,7 +119,7 @@ void loop ()
 //THIS IS THE THING THAT MAKES IT GO FORWARDS, IT SAYS -1 YOULL SEE ALL OF THE NUMBERS AND WHAT THEY DO BELOW
    
        val2 = digitalRead (ZERO);
-   if((val2 == HIGH) && (old_val2 == LOW))
+   if((val2 == LOW) && (old_val2 == HIGH))
    
    {
     newled = led = 17;
@@ -130,7 +129,7 @@ void loop ()
 //THIS MAKES THE ZERO COMMAND WORK. 17 IS WHEN NO LIGHTS ARE ON 
    
     val3 = digitalRead (RESET);
-   if((val3 == HIGH) && (old_val3 == LOW))
+   if((val3 == LOW) && (old_val3 == HIGH))
    
    {
     newled = led = 16;
@@ -140,11 +139,11 @@ void loop ()
 //RESET BACK TO STEP 1
 
      val4 = digitalRead (BACK);
-   if((val4 == HIGH) && (old_val4 == LOW))
+   if((val4 == LOW) && (old_val4 == HIGH))
    
    {
     newled = led + 1; 
-     if (newled >=17 ) { newled = 9; } 
+     if (newled >= 17 ) { newled = 9; } 
     led = newled;
    
    }
@@ -153,70 +152,88 @@ void loop ()
 
 //BELOW ARE ALL OF THE STEP BUTTONS AND WHAT THEY DO. BASICALLY WHEN THEY ARE HIT THE LED NUMBER GOES TO THE RIGHT NUMBER
 
-     vals1 = digitalRead (STEPBUTTON1);
-   if((vals1 == HIGH) && (old_vals1 == LOW))
+     vals1 = analogRead (STEPBUTTON1);
+   if((vals1 <= touchthreshold)) 
    
    {
     newled = led = 16;
+    digitalWrite(TOUCHGATE, HIGH);
+    delay(capacitivedelay);
    
    }
     
-     vals2 = digitalRead (STEPBUTTON2);
-   if((vals2 == HIGH) && (old_vals2 == LOW))
+     vals2 = analogRead (STEPBUTTON2);
+   if((vals2 <= touchthreshold))
    
    {
     newled = led = 15;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
 
-     vals3 = digitalRead (STEPBUTTON3);
-   if((vals3 == HIGH) && (old_vals3 == LOW))
+     vals3 = analogRead (STEPBUTTON3);
+   if((vals3 <= touchthreshold))
    
    {
     newled = led = 14;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
 
-     vals4 = digitalRead (STEPBUTTON4);
-   if((vals4 == HIGH) && (old_vals4 == LOW))
+     vals4 = analogRead (STEPBUTTON4);
+   if((vals4 <= touchthreshold))
    
    {
     newled = led = 13;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
    
 
-     vals5 = digitalRead (STEPBUTTON5);
-   if((vals5 == HIGH) && (old_vals5 == LOW))
+     vals5 = analogRead (STEPBUTTON5);
+   if((vals5 <= touchthreshold))
    
    {
     newled = led = 12;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
    
-     vals6 = digitalRead (STEPBUTTON6);
-   if((vals6 == HIGH) && (old_vals6 == LOW))
+     vals6 = analogRead (STEPBUTTON6);
+   if((vals6 <= touchthreshold))
    
    {
     newled = led = 11;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
 
      vals7 = analogRead (STEPBUTTON7);
-   if((vals7 >= 1000) && (old_vals7 <= 1000))
+   if((vals7 <= touchthreshold))
    
    {
     newled = led = 10;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
   
       vals8 = analogRead (STEPBUTTON8);
-   if((vals8 >= 1000) && (old_vals8 <= 1000))
+   if((vals8 <= touchthreshold))
    
    {
     newled = led = 9;
-   
+   digitalWrite(TOUCHGATE, HIGH);
+   delay(capacitivedelay);
    }
+   
+   
+
+   if((vals1 >=Gthrshld,vals2 >=Gthrshld,vals3 >=Gthrshld,vals4 >=Gthrshld,vals5 >=Gthrshld,vals6 >=Gthrshld,vals7 >=Gthrshld,vals8 >=Gthrshld))
+   {
+   digitalWrite(TOUCHGATE, LOW);
+   }
+   
+
   
   old_val = val;
   old_val1 = val1;
@@ -225,8 +242,8 @@ void loop ()
   old_val4 = val4;
 
 
-  if (newled >=17 ) { newled = 9; } 
-  if (newled <=8 ) { newled = 16; } 
+  if (newled >=17 ) { newled = 1; } 
+  if (newled <=1 ) { newled = 16; } 
   
       
 
@@ -243,6 +260,8 @@ void loop ()
     digitalWrite(STEP6, LOW);
     digitalWrite(STEP7, LOW);
     digitalWrite(STEP8, LOW);
+      digitalWrite(STEPSET1, HIGH);
+
   }
  
   if (led == 16)
@@ -254,7 +273,9 @@ void loop ()
     digitalWrite(STEP5, LOW);
     digitalWrite(STEP6, LOW);
     digitalWrite(STEP7, LOW);
-    digitalWrite(STEP8, LOW);      
+    digitalWrite(STEP8, LOW);
+      digitalWrite(STEPSET1, HIGH);
+    
   }
  if (led == 15)
   {
@@ -266,6 +287,8 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+
          }
  if (led == 14)
   {
@@ -277,6 +300,8 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+
 
   
   } 
@@ -290,6 +315,8 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+   
 
   } 
    if (led == 12)
@@ -302,6 +329,8 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+
 
   } 
    if (led == 11)
@@ -314,6 +343,8 @@ void loop ()
      digitalWrite(STEP6, HIGH);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+
 
   } 
      if (led == 10)
@@ -326,6 +357,8 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, HIGH);
      digitalWrite(STEP8, LOW);
+       digitalWrite(STEPSET1, HIGH);
+
 
   } 
        if (led == 9)
@@ -338,9 +371,11 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, HIGH);
+       digitalWrite(STEPSET1, HIGH);
+
 
   } 
-   if (led == 8)
+   if (led == 0)
   {
     digitalWrite(STEP1, LOW);
     digitalWrite(STEP2, LOW);
@@ -350,7 +385,122 @@ void loop ()
      digitalWrite(STEP6, LOW);
      digitalWrite(STEP7, LOW);
      digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, HIGH);
+
 
   } 
+
+  if (led == 8)
+  {
+    digitalWrite(STEP1, HIGH);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+    digitalWrite(STEP4, LOW);
+    digitalWrite(STEP5, LOW);
+    digitalWrite(STEP6, LOW);
+    digitalWrite(STEP7, LOW);
+    digitalWrite(STEP8, LOW);
+     digitalWrite(STEPSET1, LOW);
+    
+  }
+ if (led == 7)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, HIGH);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+         }
+ if (led == 6)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, HIGH);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+
+  
+  } 
+ if (led == 5)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, HIGH);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+
+  } 
+   if (led == 4)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, HIGH);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+
+  } 
+   if (led == 3)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, HIGH);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+
+  } 
+     if (led == 2)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, HIGH);
+     digitalWrite(STEP8, LOW);
+         digitalWrite(STEPSET1, LOW);
+
+
+  } 
+       if (led == 1)
+  {
+    digitalWrite(STEP1, LOW);
+    digitalWrite(STEP2, LOW);
+    digitalWrite(STEP3, LOW);
+     digitalWrite(STEP4, LOW);
+     digitalWrite(STEP5, LOW);
+     digitalWrite(STEP6, LOW);
+     digitalWrite(STEP7, LOW);
+     digitalWrite(STEP8, HIGH);
+         digitalWrite(STEPSET1, LOW);
+   
+
+  } 
+
 
 }
